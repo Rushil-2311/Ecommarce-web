125d86b1e791ae8d27cd8ceecdef1cedae623110
Rushil-2311 <rushilpatellinux2gmail.com>
2022-02-12 09:11:04 +0530

    friend-component-added

---
+++ src/components/friend/friend.dao.js
@@ -0,0 +1,72 @@
+const Friends = require('./friend.model');
+
+const buildSaveRequestJson = props => {
+  const json = {};
+  json.user = props.user;
+  json.friend = props.friend;
+  return json;
+};
+
+module.exports.friendExist = async props => {
+  try {
+    const frdExist = Friends
+    .findOne({ user: props.user, friend: props.friend})
+    return frdExist
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports.addFriend = async props => {
+  try {
+    const user = new Friends(buildSaveRequestJson(props));
+    const result = await user.save()
+    return result
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports.getUserFriends = async (userId, props) => {
+  try {
+    const { limit = 10, skip = 0 } = props
+    const users = Friends
+    .find({ user: userId}).select('friend')
+    .populate('friend')
+    .limit(Number(limit))
+    .skip(Number(skip))
+    .exec();
+    return users
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports.removeFriend = async ({user, friend}) => {
+  try {
+    const friendDeleted = await Friends.findOneAndDelete({ user, friend })
+    return friendDeleted
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports.getFriendsIds = async (props) => {
+  try {
+    const friends = await Friends
+    .find(props).lean()
+    return friends
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports.getFriendsCount = async (props) => {
+  try {
+    const friends = await Friends
+    .count(props)
+    return friends
+  } catch (error) {
+    throw error;
+  }
+}
